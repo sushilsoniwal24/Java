2ed275e786904e5494530ae8359f033c77cccd61
Sushil Soniwal <soniwalsushil24@gmail.com>
2023-06-02 09:38:05 +0530

    Switch Statements In Java

---
+++ T_06_Switch_Statements.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class T_06_Switch_Statements {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter A Colour :- Red, Yellow, Green");
+        String color = in.next();
+        in.close();
+
+        // Todo :- Old Syntax For Switch Statements.
+        switch (color) {
+            case "Red":
+                System.out.println("It's A Red Apple");
+                break;
+
+            case "Yellow":
+                System.out.println("It's A Yellow Banana");
+                break;
+
+            case "Green":
+                System.out.println("It's A Green Watermelon");
+                break;
+
+            default:
+                System.out.println("Not Avalible");
+        }
+
+        // Todo:- New Syntax For Switch Statements.
+        switch (color) {
+            case "Red" -> System.out.println("It's A Red Apple");
+            case "Yellow" -> System.out.println("It's A Yellow Banana");
+            case "Green" -> System.out.println("It's A Green Watermelon");
+            default -> System.out.println("Not Avalible");
+        }
+    }
+}
